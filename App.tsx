43f78e4158d923f50ada57702c2415f92b47a1d1
mali4u <malin.leander99@gmail.com>
2023-07-12 14:22:54 +0200

    External stylesheet working with google fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import externalStyle from './style/externalStyle';
+//import * as SplashScreen from 'expo-splash-screen';
+
+import { Nunito_600SemiBold} from '@expo-google-fonts/nunito';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font/build/FontHooks';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Nunito_600SemiBold
+  });
+
+  /*const onLaycoutRootView = useCallback(async () =>{
+      if(fontsLoaded){
+        await SplashScreen.hideAsync();
+      }
+  }, [fontsLoaded]);*/
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello! here is my app!</Text>
+      <Text style={[externalStyle.H2, externalStyle.purple]}>Open up App.js to start working on your app!</Text>
+      <Text style={externalStyle.P}>Hello! here is my app!</Text>
     </View>
   );
 }
---
+++ style/externalStyle.tsx
@@ -0,0 +1,54 @@
+import {StyleSheet} from 'react-native'
+
+const externalStyle = StyleSheet.create({
+
+    black:{
+        color: '#20212C'
+    },
+    white:{
+        color: '#FEFEFE'
+    },
+    grey:{
+        color: '#6B6E79'
+    },
+    purple:{
+        color: '#7052E0'
+    },
+    orange:{
+        color: '#FB8B24'
+    },
+    green:{
+        color: '#ADFC92'
+    },
+    darkBlue: {
+        color: '#312463'
+    },
+    
+    H1:{
+        fontSize: 56,
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    H2:{
+        fontSize:36,
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    H3:{
+        fontSize:20,
+        fontFamily: 'Nunito_600SemiBold'
+    },
+    H4:{
+        fontSize:16,
+        fontFamily: 'Nunito_600SemiBold'
+    },
+    P:{
+        fontSize:16,
+        fontFamily: 'Poppins_400Regular'
+    },
+    P2:{
+        fontSize:12,
+        fontFamily: 'Poppins_400Regular'
+    }
+
+})
+
+export default externalStyle;
